6668b13d794eeb4ffa165b9f011979cde700e639
Andre LaFleur <andre.lafleur@utah.edu>
2018-02-24 08:28:02 -0700

    Dinking around

--- exercise2/Protocol1Attack.java
+++ exercise2/Protocol1Attack.java
@@ -9,8 +9,10 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Protocol1Attack {
-	
+
 	private static final String ENCRYPTED_SESSION_KEY = "3fcd9e51c9705df150716f6920b08338740083d3a525cf4c4a044b8d20fb1f0";
+	private static final String hostName = "127.0.0.1";
+	private static final int portNumber = 11337;
 
 	// By Wireshark, we know that here we have the encrypted session key:
 		// 3fcd9e51c9705df150716f6920b08338740083d3a525cf4c4a044b8d20fb1f0
@@ -25,9 +27,28 @@ public class Protocol1Attack {
 		// 8740083d3a525cf4c4a044b8d20fb1f0e679b7f569281ea8bc688b888dc5c3b573162c9e2dbb5bb94fc88ea4b0f4fc6f89a3a0690ff3dca409ca0e7f48070d185fbc4b1c614dcce402cc5a26e485120c
 
 	public static void main(String[] args) {
+		try {
+			Socket connection = new Socket(hostName, portNumber);
+			InputStream input = connection.getInputStream();
+			OutputStream output = connection.getOutputStream();
+
+			output.write(hexStringToByteArray(ENCRYPTED_SESSION_KEY));
+			output.flush();
+
+			byte[] returned = new byte[112];
+			input.read(returned);
+			connection.close();
+			byteArrayToHexString(returned);
+		} catch(Exception e){
+			e.printStackTrace();
+			System.exit(1);
+		}
 		
 	}
 
+	/**
+	 * Stolen from Protocol1Server.java
+	 */
 	private static byte[] hexStringToByteArray(String s) {
 		int len = s.length();
 		byte[] data = new byte[len / 2];
@@ -37,4 +58,23 @@ public class Protocol1Attack {
 		}
 		return data;
 	}
+
+	/**
+	 * Stolen from Protocol1Server.java
+	 */
+	private static String byteArrayToHexString(byte[] data) { 
+		StringBuffer buf = new StringBuffer();
+		for (int i = 0; i < data.length; i++) { 
+			int halfbyte = (data[i] >>> 4) & 0x0F;
+			int two_halfs = 0;
+			do { 
+				if ((0 <= halfbyte) && (halfbyte <= 9)) 
+					buf.append((char) ('0' + halfbyte));
+				else 
+					buf.append((char) ('a' + (halfbyte - 10)));
+				halfbyte = data[i] & 0x0F;
+			} while(two_halfs++ < 1);
+		} 
+		return buf.toString();
+	} 
 }
